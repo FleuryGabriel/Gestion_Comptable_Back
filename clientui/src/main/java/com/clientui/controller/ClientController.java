ea0cd3f4b663afc34b7ce814652fff554b65cfc6
thomas-martin-adaming <gmartinthomas7@gmail.com>
2020-11-24 09:57:56 +0100

    Controller client UI and client service

---
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -0,0 +1,15 @@
+package com.clientui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@Controller
+@CrossOrigin
+public class ClientController {
+
+}
+
+
+
+
